179c4b2962282526c2ec3878e7cb09c15a3944d3
Mason <qq2087698086@gmail.com>
2023-12-17 01:49:38 +0800

    完成主要界面 待细化

--- device_addr.py
+++ device_addr.py
@@ -4,8 +4,32 @@
 # @Author  : 梦泽
 # @File    : device_addr.py
 # @Software: PyCharm
+import sys
 
 from tool import pyvisa_addr, serial_addr
-import tkinter as tk
-from tkinter import ttk
 
+from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtGui import QIcon
+
+
+class DeviceAddr:
+    def __init__(self):
+        self.ui = QUiLoader().load('ui/mian_window.ui')
+        self.ui.setWindowIcon(QIcon('ui/image/DeviceAddr.png'))
+        self.ui.setFixedSize(self.ui.width(), self.ui.height())
+        self.ui.AllButton.clicked.connect(pyvisa_addr)
+        self.ui.ComButton.clicked.connect(serial_addr)
+        # self.ui.show()
+
+    # def pyvisa_addr(self):
+    #     self.ui.textBrowser.setText(str(pyvisa_addr.resource_addr_desc()))
+    #
+    # def serial_addr(self):
+    #     self.ui.textBrowser.setText(str(serial_addr.get_port_desc()))
+
+
+app = QApplication(sys.argv)
+stats = DeviceAddr()
+stats.ui.show()
+app.exec()
